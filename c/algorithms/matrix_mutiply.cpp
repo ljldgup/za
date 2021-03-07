bd40ffc55c91683a508edea45f817b4f9a67d9e8
李嘉亮 <1142509832@qq.com>
2021-03-08 01:01:34 +0800

    内存一次性申请释放，速度有所提升，但是仍然没有普通算法快。。。

--- c/algorithms/matrix_mutiply.cpp
+++ c/algorithms/matrix_mutiply.cpp
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<time.h>
 #define random(x) (rand()%x)
-
+int count = 0;
 void matrix_print(int *matrix, int length){
 	for(int i = 0; i < length; i++){
 		for(int j = 0; j < length; j++){
@@ -10,34 +10,30 @@ void matrix_print(int *matrix, int length){
 		}
 		printf("\n");
 	}
-	printf("-----------------------------\n");
+	//count++;
+	//printf("%d-----------------------------\n", count);
 }
 
 
-int* matrix_add(int * matrix1, int *matrix2,  int length){
-	int * matrix3 = (int *)malloc(sizeof(int)*length*length);
+void matrix_add(int *matrix1, int *matrix2,int *matrix3,  int length){
 	for(int i = 0; i < length; i++){
 		for(int j = 0; j < length; j++){
 			*(matrix3 + i*length + j) = *(matrix1 + i*length +j) + *(matrix2 + i*length +j);
 		}
 	}
 	//matrix_print(matrix3,length);
-	return matrix3;
 }
 
-int* matrix_substract(int * matrix1, int *matrix2, int length){
-	int * matrix3 = (int *)malloc(sizeof(int)*length*length);
+void matrix_substract(int * matrix1, int *matrix2, int *matrix3, int length){
 	for(int i = 0; i < length; i++){
 		for(int j = 0; j < length; j++){
 			*(matrix3 + i*length + j) = *(matrix1 + i*length +j) - *(matrix2 + i*length +j);
 		}
 	}
 	//matrix_print(matrix3,length);
-	return matrix3;
 }
 
-int *matrix_copy(int *matrix1, int x1, int y1, int length, int copy_length){
-	int * matrix3 = (int *)malloc(sizeof(int) * copy_length * copy_length);
+void matrix_copy(int *matrix1, int x1, int y1, int length, int * matrix3, int copy_length){
 	for(int i = 0; i < copy_length; i++){
 		for(int j = 0; j < copy_length; j++){
 			//printf("%d %d", i, j);
@@ -49,8 +45,7 @@ int *matrix_copy(int *matrix1, int x1, int y1, int length, int copy_length){
 				 *(matrix3 + i*copy_length + j) = 0;
 			}
 		}
-	} 
-	return matrix3;
+	}
 }
 
 void matrix_init(int *matrix, int length){
@@ -62,8 +57,7 @@ void matrix_init(int *matrix, int length){
 }
 
 
-int* normal_square_matrix_mutiply(int *matrix1,int *matrix2, int length){
-	int *matrix3 = (int*)malloc(sizeof(int) * length * length);
+void normal_square_matrix_mutiply(int *matrix1,int *matrix2, int *matrix3, int length){
 	for(int i = 0;i < length; i++){
 		for(int j = 0; j < length; j++){
 			*(matrix3 + i*length + j) = 0;
@@ -74,27 +68,65 @@ int* normal_square_matrix_mutiply(int *matrix1,int *matrix2, int length){
 			//printf("\n");
 		}
 	}
-	return matrix3;
 }
 
 //可能不能反复拷贝，释放反而很慢。。。
-int * Strassen(int *matrix1, int *matrix2, int length){
+void Strassen(int *matrix1, int *matrix2, int *matrix3, int length){
+
 	
 	if (length == 1){
-		int *result = (int *)malloc(sizeof(int) * 1);
-		*result = *matrix1 * *matrix2;
-		return result;
-	}
-
+		*matrix3 = *matrix1 * *matrix2;
+		return;
+	} 
+	
+	
 	int copy_length = length/2 + length%2;
-	int *A11 = matrix_copy(matrix1, 0, 0, length, copy_length);
- 	int *A21 = matrix_copy(matrix1, copy_length, 0, length, copy_length);
-	int *A12 = matrix_copy(matrix1, 0, copy_length, length, copy_length);
-	int *A22 = matrix_copy(matrix1, copy_length, copy_length, length, copy_length);
-	int *B11 = matrix_copy(matrix2, 0, 0, length, copy_length);
-	int *B21 = matrix_copy(matrix2, copy_length, 0, length, copy_length);
-	int *B12 = matrix_copy(matrix2, 0, copy_length, length, copy_length);
-	int *B22 = matrix_copy(matrix2, copy_length, copy_length, length, copy_length);
+	int size = copy_length * copy_length;
+	int *result = (int *)malloc(sizeof(int)*size*35);
+	
+	int *A11 = result + size*0;
+	int *A21 = result + size*1;
+	int *A12 = result + size*2;
+	int *A22 = result + size*3;
+	int *B11 = result + size*4;
+	int *B21 = result + size*5;
+	int *B12 = result + size*6;
+	int *B22 = result + size*7;
+	int *S1 = result + size*8;
+	int *S2 = result + size*9;
+	int *S3 = result + size*10;
+	int *S4 = result + size*11;
+	int *S5 = result + size*12;
+	int *S6 = result + size*13;
+	int *S7 = result + size*14;
+	int *S8 = result + size*15;
+	int *S9 = result + size*16;
+	int *S10 = result + size*18;
+	int *P1 = result + size*19;
+	int *P2 = result + size*20;
+	int *P3 = result + size*21;
+	int *P4 = result + size*22;
+	int *P5 = result + size*24;
+	int *P6 = result + size*25;
+	int *P7 = result + size*26;
+	int *T1 = result + size*27;
+	int *T2 = result + size*28;
+	int *T3 = result + size*29;
+	int *T4 = result + size*30;
+	int *C11 = result + size*31;
+	int *C12 = result + size*32;
+	int *C21 = result + size*33;
+	int *C22 = result + size*34;
+	
+
+	 matrix_copy(matrix1, 0, 0, length, A11, copy_length);
+ 	 matrix_copy(matrix1, copy_length, 0, length, A21, copy_length);
+	 matrix_copy(matrix1, 0, copy_length, length, A12, copy_length);
+	 matrix_copy(matrix1, copy_length, copy_length, length, A22, copy_length);
+	 matrix_copy(matrix2, 0, 0, length, B11, copy_length);
+	 matrix_copy(matrix2, copy_length, 0, length, B21, copy_length);
+	 matrix_copy(matrix2, 0, copy_length, length, B12, copy_length);
+	 matrix_copy(matrix2, copy_length, copy_length, length, B22, copy_length);
 /* 	matrix_print(A11, copy_length);
 	matrix_print(A12, copy_length);
 	matrix_print(A21, copy_length);
@@ -103,23 +135,16 @@ int * Strassen(int *matrix1, int *matrix2, int length){
 	matrix_print(B12, copy_length);
 	matrix_print(B21, copy_length);
 	matrix_print(B22, copy_length); */
-	int *S1 = matrix_substract(B12, B22, copy_length);
-	int *S2 = matrix_add(A11, A12, copy_length);
-	int *S3 = matrix_add(A21, A22, copy_length);
-	int *S4 = matrix_substract(B21 , B11, copy_length);
-	int *S5 = matrix_add(A11 , A22, copy_length);
-	int *S6 = matrix_add(B11 , B22, copy_length);
-	int *S7 = matrix_substract(A12 , A22, copy_length);
-	int *S8 = matrix_add(B21, B22, copy_length);
-	int *S9 = matrix_substract(A11, A21, copy_length);
-	int *S10 = matrix_add(B11 , B12, copy_length);
-	int *P1 = Strassen(A11,S1, copy_length);
-	int *P2 = Strassen(S2,B22, copy_length);
-	int *P3 = Strassen(S3, B11, copy_length);
-	int *P4 = Strassen(A22,S4, copy_length);
-	int *P5 = Strassen(S5, S6, copy_length);
-	int *P6 = Strassen(S7, S8, copy_length);
-	int *P7 = Strassen(S9, S10, copy_length);
+	 matrix_substract(B12, B22, S1, copy_length);
+	 matrix_add(A11, A12, S2, copy_length);
+	 matrix_add(A21, A22, S3, copy_length);
+	 matrix_substract(B21 , B11, S4, copy_length);
+	 matrix_add(A11 , A22, S5, copy_length);
+	 matrix_add(B11 , B22, S6, copy_length);
+	 matrix_substract(A12 , A22, S7, copy_length);
+	 matrix_add(B21, B22, S8, copy_length);
+	 matrix_substract(A11, A21, S9, copy_length);
+	 matrix_add(B11 , B12, S10, copy_length);
 /* 	matrix_print(S1, copy_length);
 	matrix_print(S2, copy_length);
 	matrix_print(S3, copy_length);
@@ -129,74 +154,77 @@ int * Strassen(int *matrix1, int *matrix2, int length){
 	matrix_print(S7, copy_length);
 	matrix_print(S8, copy_length);
 	matrix_print(S9, copy_length);
-	matrix_print(S10, copy_length); 
-	matrix_print(P1, copy_length);
+	matrix_print(S10, copy_length);  */
+	
+	 Strassen(A11,S1, P1, copy_length);
+	 Strassen(S2,B22, P2, copy_length);
+	 Strassen(S3, B11, P3, copy_length);
+	 Strassen(A22,S4, P4, copy_length);
+	 Strassen(S5, S6, P5, copy_length);
+	 Strassen(S7, S8, P6, copy_length);
+	 Strassen(S9, S10, P7, copy_length);
+
+/* 	matrix_print(P1, copy_length);
 	matrix_print(P2, copy_length);
 	matrix_print(P3, copy_length);
 	matrix_print(P4, copy_length);
 	matrix_print(P5, copy_length);
 	matrix_print(P6, copy_length);
-	matrix_print(P7, copy_length);*/
+	matrix_print(P7, copy_length); */
 
-	int *t1, *t2;
-	t1 = matrix_add(P5, P4, copy_length);
-	t2 = matrix_substract(t1, P2, copy_length);
-	int *C11 = matrix_add(t2, P6, copy_length);
-	free(t1);free(t2);
-	int *C12 = matrix_add(P1, P2, copy_length);
-	int *C21 = matrix_add(P3, P4, copy_length);
-	t1 = matrix_add(P5, P1, copy_length);
-	t2 = matrix_substract(t1, P3, copy_length);
-	int *C22 = matrix_substract(t2, P7, copy_length);
-	free(t1);free(t2);
-/* 	matrix_print(C11, copy_length);
+	matrix_add(P5, P4, T1, copy_length);
+	matrix_substract(T1, P2, T2, copy_length);
+	matrix_add(T2, P6, C11, copy_length);
+	matrix_add(P1, P2, C12, copy_length);
+	matrix_add(P3, P4, C21, copy_length);
+	matrix_add(P5, P1, T3, copy_length);
+	matrix_substract(T3, P3, T4, copy_length);
+	matrix_substract(T4, P7, C22, copy_length);
+/*  	matrix_print(C11, copy_length);
 	matrix_print(C12, copy_length);
 	matrix_print(C21, copy_length);
-	matrix_print(C22, copy_length); */
+	matrix_print(C22, copy_length);  */
 	
 	//matrix_print(C11, copy_length);
-	int *result = (int *)malloc(sizeof(int) * length * length);
  	//copy
 	for(int i = 0; i < copy_length; i++){
 		for(int j = 0; j < copy_length; j++){
-			*(result + i * length + j) = *(C11 + i*copy_length + j);
+			*(matrix3 + i * length + j) = *(C11 + i*copy_length + j);
 		}
 	}
 
 	for(int i = 0; i < copy_length; i++){
 		for(int j = copy_length; j < length; j++){
-			*(result + i * length + j) = *(C12 + i*copy_length + j-copy_length);
+			*(matrix3 + i * length + j) = *(C12 + i*copy_length + j-copy_length);
 		}
 	}
 
 	for(int i = copy_length; i < length; i++){
 		for(int j = 0; j < copy_length; j++){
-			*(result + i * length + j) = *(C21 + (i-copy_length)*copy_length + j);
+			*(matrix3 + i * length + j) = *(C21 + (i-copy_length)*copy_length + j);
 		}
 	}
 
 	for(int i = copy_length; i < length; i++){
 		for(int j = copy_length; j < length; j++){
-			*(result + i * length + j) = *(C22 + (i-copy_length)*copy_length +  j-copy_length);
+			*(matrix3 + i * length + j) = *(C22 + (i-copy_length)*copy_length +  j-copy_length);
 		}
 	}
 	
 	//free
- 	free(A11);free(A21);free(A12);free(A22);free(B11);free(B21);free(B12);free(B22);
-	free(S1);free(S2);free(S3);free(S4);free(S5);free(S6);free(S7);free(S8);free(S9);free(S10);
-	free(P1);free(P2);free(P3);free(P4);free(P5);free(P6);free(P7); 
-
-	return result;
-
+ 	free(result);
 }
 
 int main(int argc,char *argv[])
 {
 	srand((int)time(0));
-	int length = 500; 
+	int length;
+	scanf("%d", &length);
+	
 	int *matrix1 = (int*)malloc(sizeof(int)*length*length);
 	int *matrix2 = (int*)malloc(sizeof(int)*length*length);
-
+	int *matrix3 = (int*)malloc(sizeof(int)*length*length);
+	int *matrix4 = (int*)malloc(sizeof(int)*length*length);
 	
 	double endtime;
 	clock_t start,end;
@@ -206,13 +234,13 @@ int main(int argc,char *argv[])
 	
 	
 	start=clock();		//程序开始计时
-	int *matrix3 = normal_square_matrix_mutiply(matrix1, matrix2, length);
+	normal_square_matrix_mutiply(matrix1, matrix2, matrix3, length);
 	end=clock();
 	endtime = (double)(end-start)/CLOCKS_PER_SEC;
 	printf("normal time %f \n", endtime);
 	
 	start=clock();	
-	int *matrix4 = Strassen(matrix1, matrix2, length);
+	Strassen(matrix1, matrix2, matrix4, length);
 	end=clock();
 	endtime = (double)(end-start)/CLOCKS_PER_SEC;
 	printf("Strassen time %f \n", endtime);
