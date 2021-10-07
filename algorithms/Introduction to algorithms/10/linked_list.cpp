52b7884b9680a0678561fb834885f6d2a09119a4
ljldgup <1142509832@qq.com>
2021-10-07 23:28:23 +0800

    lru模拟

--- algorithms/Introduction to algorithms/10/linked_list.cpp
+++ algorithms/Introduction to algorithms/10/linked_list.cpp
@@ -8,6 +8,7 @@
 #include<iostream>
 #include<vector>
 #include<ctime>
+#include<unordered_map>
 using namespace std;
 #define random(x) (rand()%(x + 1))
 
@@ -16,6 +17,7 @@ class Node{
 		Node *next;
 		Node *prev;
 		int value;
+        long time;
 		Node(int v){
 			value = v;
 		}
@@ -23,14 +25,14 @@ class Node{
 
 class LinkedList{
 	private:
-		int length;
+		int size;
 		Node *nil;
 	public:
 		LinkedList(){
 			nil = new Node(-10000000);
 			nil->next = nil;
 			nil->prev = nil;
-			length = 0;
+			size = 0;
 		}
 
 		Node* search(int k){
@@ -59,15 +61,34 @@ class LinkedList{
 			cout<<node->value<<endl;
 			insert(node, v);
 		}
+        
+        Node *insertTail(int v){
+			cout<<"insert "<<v<<" postion "<<size<<endl;
+			Node *node = new Node(v);
+            return insert(nil->next, v);
+		}
+        
+        int removeTail(){
+			Node *tail = nil->prev;
+            if(tail != nil){
+                int v = tail->value;
+                cout<<"remove tail "<<v<<endl;
+                remove(tail);
+                return v;
+            }
+            return -1;
+		}
 		
-		void insert(Node *pos, int v){
+		Node * insert(Node *pos, int v){
 			Node *node = new Node(v);
 			node->prev = pos->prev;
 			node->next = pos;
 			node->prev->next = node;
 			pos->prev = node;
-			length++;
+			size++;
+            return node;
 		}
+
 
 		void remove(int n){
 			cout<<"remove postion "<<n<<endl;
@@ -76,14 +97,29 @@ class LinkedList{
 			remove(node);
 		}
 		
-		void remove(Node *pos){
-			pos->prev->next = pos->next;
-			pos->next->prev = pos->prev;
-			length--;
+		void remove(Node *cur){
+			cur->prev->next = cur->next;
+			cur->next->prev = cur->prev;
+			size--;
+            delete cur;
+		}
+        
+        void update(Node *cur){
+            cout<<"update :"<<cur->value<<endl;
+            
+			cur->prev->next = cur->next;
+			cur->next->prev = cur->prev;
+			
+            Node *head = nil->next;
+            nil->next = cur;
+            cur->prev = nil;
+            
+            cur->next = head;
+            head->prev = cur;
 		}
 		
 		void print(){
-			cout<<"length:"<<length<<endl;
+			cout<<"size: "<<size<<endl;
 			Node *head = nil->next;
 			while(head!= nil){
 				cout<<head->value<<' ';
@@ -91,6 +127,10 @@ class LinkedList{
 			}
 			cout<<endl;
 		}
+        
+        int getSize(){
+            return size;
+        }
 		
 		~LinkedList(){
 			Node *head = nil->next;
@@ -105,6 +145,26 @@ class LinkedList{
 		}
 };
 
+void LRUTest(){
+    cout<<"test lru"<<endl;
+    LinkedList linkedList;
+    unordered_map<int, Node*> cache;
+    for(int i = 0; i < 20; i++){
+        int v = random(10);
+        if(cache.find(v) == cache.end()){
+            if(linkedList.getSize() >= 8){
+                int oldValue = linkedList.removeTail();
+                cache.erase(cache.find(oldValue));
+            }
+            Node* n = linkedList.insertTail(v);
+            cache[v] = n;
+        }
+		else linkedList.update(cache[v]);
+        linkedList.print();
+	}
+    
+}
+
 int main(){
 	LinkedList linkedList;
 	for(int i = 0; i < 20; i++){
@@ -115,6 +175,8 @@ int main(){
 			linkedList.remove(random(i));
 		}
 	}
+    
+    LRUTest();
 }
 
 
