7bf206d9e471b1e8ddc773dc7cacf0c823c7a89d
ljldgup <1142509832@qq.com>
2022-06-17 00:56:05 +0800

    -

--- lisp/discrete_mathematics/13.clj
+++ lisp/discrete_mathematics/13.clj
@@ -9,89 +9,104 @@
         (if (not (empty? signals))
             (let [signal (first signals)
                   [output new-status] (status signal)]
-                (println signal output)
+;                (println signal output)
                 (recur (rest signals) new-status))))
                 
 ;不用loop的遍历方案          
 (defn status-reducer[status signal]
     (let [[output new-status] (status signal)]
-        (println signal output)
+;        (println signal output)
         new-status))
 (reduce status-reducer s0 input)
 
 
 
 ;自动机器正则匹配
-;匹配结束
-(defn final-status[string] '( \n ) )
-(final-status "123123")
 
-    
-(defn ch-match? [raw-ch pattern-chs]
+;单个字符匹配  
+(defn ch-match? [pattern-chs raw-ch]
+;    (println 'ch-match? pattern-chs raw-ch )
     (if (or (= pattern-chs :any-ch) (pattern-chs raw-ch))
         true
         false))
 
-
-
-
 ;固定长度匹配，返回lamdba，传入下一个状态生成匹配函数
 (defn fixed-match-status [pattern-chs length]
-    ;(println pattern-chs length)
+;    (println 'fixed-match-status pattern-chs length)
     (fn[next-status]
         ;(println next-status)
         (fn[string]
-            (println pattern-chs string)
+;            (println pattern-chs string)
             (if (< (count string) length) 
                 nil
                 (let [raw-str (take length string)
                       rest-string (subs string length)]
-                    (if (every? #(ch-match? % pattern-chs)  raw-str)
+                    (if (every? #(ch-match? pattern-chs %)  raw-str)
                         (if-let[rest-matched-str (next-status rest-string)]
                             (concat raw-str rest-matched-str))))))))
 							
 ;单个匹配，返回lamdba，传入下一个状态生成匹配函数
 (defn single-match-status [pattern-chs]
     (fixed-match-status pattern-chs 1))
-						
-(defmulti gen-status (fn[& args](if (= :or (first args) :or (keyword (str "and" (count args)))))))
+
+;匹配结束
+(defn final-status[string] '( \n ) )
 
 ;把status作为raw-status下一个调用的匹配器
-(defn assembly-status[status raw-status]
-    (println status raw-status)
+(defn assembly-status[raw-status status]
+    ;(println raw-status status)
     (raw-status status))
 
+(defn choose-generator[args]
+;        (println args (first args) (keyword? (first args)))
+        (if (keyword? (first args))
+            (first args) 
+            (count args)))
+        
+(defmulti gen-status choose-generator)
+
 ;并行化状态，用于或
-(defn parallel-status[& statuses]
+(defn or-assembly-status[parsed-patterns]
+;    (println 'or-assembly-status parsed-patterns)
     (fn[next-status]
+        ;把每个过程和下一个过程串联
         (let [new-statuses (map 
                                 #(assembly-status %1 next-status) 
-                                (map gen-statuses statues))]
+                                (map gen-status parsed-patterns))]
             (fn[string]
-                (some #(% string) new-status)))))
-
+                ;返回第一个能匹配的结果，失败继续尝试下次，实现回溯
+                (some #(% string) new-statuses)))))
+                
+(defn serial-status-assemble [parsed-patterns]
+;    (println 'serial-assembly-status parsed-patterns)
+    (fn[next-status]
+        (reduce 
+            #(assembly-status %2 %1) 
+            next-status
+            (reverse (map gen-status parsed-patterns)))))
+                
 ;不是or就是and，and根据入参确定是单个还是定长，还是范围
-(defmethod gen-status :or [pattern-chs]
-    (single-match-status pattern-chs))
-(defmethod gen-status :and1 [pattern-chs]
-    (single-match-status pattern-chs))
-(defmethod gen-status :and2 [pattern-chs length]
-    (fixed-match-status pattern-chs length))
+(defmethod gen-status :or [patterns]
+    (or-assembly-status (rest patterns)))
+(defmethod gen-status :serial [patterns]
+    (serial-status-assemble (rest patterns)))
+(defmethod gen-status 1 [patterns]
+    (apply single-match-status patterns))
+(defmethod gen-status 2 [patterns]
+    (apply fixed-match-status patterns))
 
 
+(((gen-status '(#{\a})) final-status) "a")
+(((gen-status '(#{\a} 2)) final-status)  "aab")
+(((gen-status '(#{\a} 2)) final-status)  "ab")
+(((gen-status '(:serial (#{\a} 2) (#{\b})))  final-status) "aabc")
+(((gen-status '(:or (#{\a} 2) (#{\b}))) final-status) "aabc")
+(((gen-status '(:or (#{\a} 2) (#{\b}))) final-status) "babc")
 
+(def patterns '(:serial (#{\a} 2) (:or (#{\d \c \b}) (#{\e} 3)) (#{\f} 2)))
+(def regular-match-machine ((gen-status patterns) final-status))
 
-
-(gen-status \a)
-(gen-status \a 2)
-
-(def parsed-pattern '((#{\a} 2) (:or (#{\d \c \b}) (#{\e} 3))) (#{\a}))
-(def regular-match-machine (reduce 
-                #(assembly-status %1 %2) 
-                final-status 
-                (reverse (map #(apply gen-status %) parsed-pattern))))
-
-(regular-match-machine  "aabsadfwe")
-(regular-match-machine  "absadfwe")
-(regular-match-machine  "aaccccc")
-(regular-match-machine  "aadef")
+(regular-match-machine  "aabffwe")
+(regular-match-machine  "aaaffwe")
+(regular-match-machine  "aaeeeffwe")
+(regular-match-machine  "aaeeffwe")
