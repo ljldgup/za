98f4cc88656b18a222222e0eed5ffa02f2fa91c7
ljldgup <1142509832@qq.com>
2022-06-24 23:27:40 +0800

    -

--- lisp/discrete-mathematics/4.clj
+++ lisp/discrete-mathematics/4.clj
@@ -12,3 +12,36 @@
         (println a "*" x "+" b "*" y "=" r)))
         
 (vis_gcd 24 42)
+
+;复平方求模
+(defn exp-mod[b n m]
+    ;(println b n m)
+    (cond 
+        (= n 0) 1
+        (= m 1) 0
+        :else 
+            (let [half (exp-mod b (quot n 2) m)]
+                ;(println half)
+                (if (= (mod n 2) 1)
+                    (-> (* half half) (* b) (mod m))
+                    (-> (* half half) (mod m))))))
+(exp-mod 3 644 645)
+
+;递归定义的随机数生成器 7 4 9可以得到随机数
+(take 10 (iterate #(mod (+ (* % 7)  4) 9) 3))
+;7 4 6不行，周期只有3
+(take 10 (iterate #(mod (+ (* % 7)  6) 9) 3))
+
+(defn random-generator-test[m]
+    ;初始值0
+    (doseq [a (range 1 m) b (range 1 m)]
+        (let [nums (take m (iterate #(mod (+ (* % a)  b) m) 0))]
+            (if (= (count (distinct nums)) m)
+                (do 
+                    (println a b m)
+                    (doseq [x nums] (print x " "))
+                    (println))))))
+;基本上a b 和m公约数都是1
+;m是奇数效果更好
+(random-generator-test 9)
+(random-generator-test 11)
