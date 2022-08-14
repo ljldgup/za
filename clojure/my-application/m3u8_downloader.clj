f0f1e1d8092953d6b2e2997e921db83eaa58a56f
18115676974 <1142509832@qq.com>
2022-08-14 16:30:10 +0800

    -

--- clojure/my-application/m3u8_downloader.clj
+++ clojure/my-application/m3u8_downloader.clj
@@ -1,49 +1,86 @@
+(defn mkdirs[path]
+    ;(println path)
+    (let [dir (java.io.File. path)]
+        (if (not (.exists dir))
+            (.mkdirs dir))))
+(mkdirs "sdf/sdf/sdf/sdf") 
+ 
+(defn exists[path]
+    ;(println path)
+    (let [dir (java.io.File. path)]
+        (.exists dir)))
+        
+(mkdirs "sdf/sdf/sdf/sdf")  
+
+
+(defn get_related_path_name[url]
+    (list (re-find #"(?<=hls/).+/(?=[^/]+$)", url) (re-find #"(?<=/)[^/]+$", url)))
+
+(get_related_path_name "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
+(get_related_path_name "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
+
+;slurp 处理二进制会出错
+(defn binary_save[url]
+    (let [net_url (java.net.URL. url)
+          connection (clojure.core/cast java.net.HttpURLConnection (.openConnection net_url))
+      [relate_path file_name] (get_related_path_name url)
+      path_name (str relate_path file_name)]
+      (mkdirs relate_path)
+      (.connect connection)
+      (if (not (exists  path_name))
+          (with-open [os (clojure.java.io/output-stream (str relate_path file_name))
+                      is (.getInputStream connection)]
+                (clojure.java.io/copy is os))
+          (println "exists" path_name))))
+(binary_save "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/6qlS2hEV.ts")
+(binary_save "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/6qlS2hEV.ts")
+
+
+(defn slurp_save_then_return[url]
+    (let [response (slurp url)
+      [relate_path file_name] (get_related_path_name url)
+      path_name (str relate_path file_name)]
+      (mkdirs relate_path)
+      (if (not (exists  path_name))
+        (do
+            (spit (str relate_path file_name) response)
+            response)
+        (do (println "exists" path_name)
+            (slurp path_name)))))
+
+(slurp_save_then_return "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
+(slurp_save_then_return "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
+
+
 (defn get_base_url[url]
     (re-find #".+/(?=[^/]+$)", url))
 
 
 (get_base_url "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
-
 (get_base_url "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
-
-(defn get_base_name[url]
-    (re-find #"[^/]+(?=/[^/]+$)", url))
-
-(get_base_name "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
 
 
 (defn get_resolution_path[origin_url]
-    (->> (slurp origin_url) (re-find #".+index.m3u8")))
+    (->> (slurp_save_then_return origin_url) (re-find #".+index.m3u8")))   
     
 (get_resolution_path "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8") 
 
 (defn get_ts_paths[resolution_url]
-    (->> (slurp resolution_url) (re-seq #".+\.ts")))
+    (->> (slurp_save_then_return resolution_url) (re-seq #".+\.ts")))
 (get_ts_paths "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
 
-(defn get_ffmpeg_cmd[ts_paths dest]
-    (format "ffmpeg -i \"%s\" -c copy %s.mp4" (str "concat:" (clojure.string/join "|" ts_paths)) dest))
-(-> (get_ts_paths "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
-    (get_ffmpeg_cmd  "test")
-    println )
+(def ffmpeg_cmd "ffmpeg -i index.m3u8 -c copy ")
+   
     
 (defn download_m3u8[url]
     (let [resolution_base_url (get_base_url url)
-          base_name (get_base_name url)
           resolution_path (get_resolution_path url)
           resolution_url (str resolution_base_url resolution_path)
           ts_base_url (get_base_url resolution_url)
           ts_paths (get_ts_paths resolution_url)]
           (println resolution_base_url resolution_url ts_base_url)
-          (spit  (str base_name "_ts_url.txt") (clojure.string/join "\n" (map #(str "file " %) ts_paths)))
-          (doseq [ts_path ts_paths]
-            
-            (if (not (.exists (java.io.File. ts_path)))
-                (do
-                    (println "downloading" ts_path)
-                    (spit ts_path (slurp (str ts_base_url ts_path)))
-                    (Thread/sleep 100))
-                (println "downloaded" ts_path)))))
+          (doseq [ts_path ts_paths]    
+                (binary_save (str ts_base_url ts_path)))))
 (download_m3u8 "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
           
          
