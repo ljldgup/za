7e30358ffcc0030edd182033314b6aa7476bb922
ljldgup <1142509832@qq.com>
2022-08-14 23:56:22 +0800

    -

--- clojure/my-application/m3u8_downloader.clj
+++ clojure/my-application/m3u8_downloader.clj
@@ -1,3 +1,11 @@
+(def basic_url "https://3bmmikh.life/new/hls")
+(defn get_url_name[url]
+    (let [respose (slurp url)]
+        (list 
+            (re-find #"[^\"]+index.m3u8"  respose) 
+            (re-find #"(?<=正在播放：)[^<]+", respose))))
+(get_url_name "https://3bmmnr0e.life/suoyoushipin/guochan/29796.html")
+        
 (defn mkdirs[path]
     ;(println path)
     (let [dir (java.io.File. path)]
@@ -14,13 +22,18 @@
 
 
 (defn get_related_path_name[url]
-    (list (re-find #"(?<=hls/).+/(?=[^/]+$)", url) (re-find #"(?<=/)[^/]+$", url)))
+    (list 
+        (re-find #"(?<=hls/).+/(?=[^/]+$)", url) 
+        (re-find #"(?<=/)[^/]+$", url)))
 
 (get_related_path_name "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
 (get_related_path_name "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
 
+
+
 ;slurp 处理二进制会出错
 (defn binary_save[url]
+    (println 'binary_save url)
     (let [net_url (java.net.URL. url)
           connection (clojure.core/cast java.net.HttpURLConnection (.openConnection net_url))
       [relate_path file_name] (get_related_path_name url)
@@ -37,6 +50,7 @@
 
 
 (defn slurp_save_then_return[url]
+    (println 'slurp_save_then_return url)
     (let [response (slurp url)
       [relate_path file_name] (get_related_path_name url)
       path_name (str relate_path file_name)]
@@ -53,6 +67,7 @@
 
 
 (defn get_base_url[url]
+    (println 'get_base_url url)
     (re-find #".+/(?=[^/]+$)", url))
 
 
@@ -60,27 +75,39 @@
 (get_base_url "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
 
 
-(defn get_resolution_path[origin_url]
+(defn get_vedio_path[origin_url]
+    (println 'get_vedio_path origin_url)
     (->> (slurp_save_then_return origin_url) (re-find #".+index.m3u8")))   
     
-(get_resolution_path "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8") 
+(get_vedio_path "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8") 
 
-(defn get_ts_paths[resolution_url]
-    (->> (slurp_save_then_return resolution_url) (re-seq #".+\.ts")))
+(defn get_ts_paths[vedio_url]
+    (->> (slurp_save_then_return vedio_url) (re-seq #".+\.ts")))
 (get_ts_paths "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
 
-(def ffmpeg_cmd "ffmpeg -i index.m3u8 -c copy ")
-   
+(defn get_ffmpeg_cmd [base_path title]
+    (format "ffmpeg -i %sindex.m3u8 -c copy %s.mp4" base_path title))
+(get_ffmpeg_cmd "sdfsd" "山擦")
     
-(defn download_m3u8[url]
-    (let [resolution_base_url (get_base_url url)
-          resolution_path (get_resolution_path url)
-          resolution_url (str resolution_base_url resolution_path)
-          ts_base_url (get_base_url resolution_url)
-          ts_paths (get_ts_paths resolution_url)]
-          (println resolution_base_url resolution_url ts_base_url)
-          (doseq [ts_path ts_paths]    
-                (binary_save (str ts_base_url ts_path)))))
-(download_m3u8 "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
+(defn download_mimei[url]
+    (let [[relate_index_url title] (get_url_name url)
+          index_url (str basic_url relate_index_url)]
+          (download_m3u8 index_url title)))
           
+
+(defn download_m3u8[url title]
+     (let [vedio_base_url (get_base_url url)
+          [local_path _] (get_related_path_name url)
+          vedio_path (get_vedio_path url)
+          vedio_url (str vedio_base_url vedio_path)
+          ts_base_url (get_base_url vedio_url)
+          ts_paths (get_ts_paths vedio_url)]
+          (map println (list url vedio_base_url vedio_url ts_base_url))
+          (spit title (get_ffmpeg_cmd local_path title))))
+          ;(doseq [ts_path ts_paths]    
+                ;(binary_save (str ts_base_url ts_path)))))
+                
+
+(download_mimei "https://3bmmnr0e.life/suoyoushipin/guochan/29796.html")
+(download_m3u8 "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")  
          
