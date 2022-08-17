66ae30f1cc00240223f213a7a5b6462bef6fcacc
ljldgup <1142509832@qq.com>
2022-08-17 23:22:53 +0800

    -

--- clojure/my-application/m3u8_downloader.clj
+++ clojure/my-application/m3u8_downloader.clj
@@ -1,6 +1,11 @@
+(def piece_count_limit 1000)
+(def rand_delay_limit 1000)
+(def out_put_file "cmd.txt")
+
+
 (def basic_url "https://3bmmikh.life/new/hls")
 (defn get_url_name[url]
-    (let [respose (slurp url)]
+    (let [respose (slurp_save_then_return url)]
         (list 
             (re-find #"[^\"]+index.m3u8"  respose) 
             (re-find #"(?<=正在播放：)[^<]+", respose))))
@@ -11,7 +16,7 @@
     (let [dir (java.io.File. path)]
         (if (not (.exists dir))
             (.mkdirs dir))))
-(mkdirs "sdf/sdf/sdf/sdf") 
+;(mkdirs "sdf/sdf/sdf/sdf") 
  
 (defn exists[path]
     ;(println path)
@@ -23,7 +28,7 @@
 
 (defn get_related_path_name[url]
     (list 
-        (re-find #"(?<=hls/).+/(?=[^/]+$)", url) 
+        (re-find #"(?<=//[^/]{0,100}/).+/(?=[^/]+$)", url) 
         (re-find #"(?<=/)[^/]+$", url)))
 
 (get_related_path_name "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
@@ -38,25 +43,28 @@
           connection (clojure.core/cast java.net.HttpURLConnection (.openConnection net_url))
       [relate_path file_name] (get_related_path_name url)
       path_name (str relate_path file_name)]
-      (mkdirs relate_path)
-      (.connect connection)
       (if (not (exists  path_name))
-          (with-open [os (clojure.java.io/output-stream (str relate_path file_name))
-                      is (.getInputStream connection)]
-                (clojure.java.io/copy is os))
-          (println "exists" path_name))))
+        (do     
+            (mkdirs relate_path)
+            (doto connection 
+                (.setConnectTimeout 5000) 
+                (.setReadTimeout 8000)
+                (.connect)) 
+              (with-open [os (clojure.java.io/output-stream (str relate_path file_name))
+                          is (.getInputStream connection)]
+                    (clojure.java.io/copy is os))
+              (println "exists" path_name)))))
 (binary_save "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/6qlS2hEV.ts")
 (binary_save "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/6qlS2hEV.ts")
 
 
 (defn slurp_save_then_return[url]
     (println 'slurp_save_then_return url)
-    (let [response (slurp url)
-      [relate_path file_name] (get_related_path_name url)
-      path_name (str relate_path file_name)]
-      (mkdirs relate_path)
-      (if (not (exists  path_name))
-        (do
+    (let [[relate_path file_name] (get_related_path_name url)
+           path_name (str relate_path file_name)]
+      (if (not (exists path_name))
+        (let [response (slurp url)]
+            (mkdirs relate_path)
             (spit (str relate_path file_name) response)
             response)
         (do (println "exists" path_name)
@@ -86,13 +94,9 @@
 (get_ts_paths "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
 
 (defn get_ffmpeg_cmd [base_path title]
-    (format "ffmpeg -i %sindex.m3u8 -c copy %s.mp4" base_path title))
+    (format "ffmpeg -i ./%sindex.m3u8 -c copy %s.mp4\r\n" base_path title))
 (get_ffmpeg_cmd "sdfsd" "山擦")
-    
-(defn download_mimei[url]
-    (let [[relate_index_url title] (get_url_name url)
-          index_url (str basic_url relate_index_url)]
-          (download_m3u8 index_url title)))
+
           
 
 (defn download_m3u8[url title]
@@ -103,11 +107,20 @@
           ts_base_url (get_base_url vedio_url)
           ts_paths (get_ts_paths vedio_url)]
           (map println (list url vedio_base_url vedio_url ts_base_url))
-          (spit title (get_ffmpeg_cmd local_path title))))
-          ;(doseq [ts_path ts_paths]    
-                ;(binary_save (str ts_base_url ts_path)))))
-                
-
-(download_mimei "https://3bmmnr0e.life/suoyoushipin/guochan/29796.html")
-(download_m3u8 "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")  
-         
+          (if (< (count ts_paths) piece_count_limit)
+              (do 
+                (spit out_put_file (get_ffmpeg_cmd local_path title) :append true)
+                (doseq [ts_path ts_paths]  
+                    ;(Thread/sleep (rand-int rand_delay_limit))
+                    (binary_save (str ts_base_url ts_path)))))))
+    
+(defn download_mimei[url]
+    (let [[relate_index_url title] (get_url_name url)
+          index_url (str basic_url relate_index_url)]
+          (download_m3u8 index_url title)))
+          
+;(download_mimei "https://3bmmnr0e.life/suoyoushipin/guochan/29796.html")
+;(download_m3u8 "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")  
+(def url_list [])
+(spit out_put_file "")
+(doseq [url url_list] (download_mimei url))
