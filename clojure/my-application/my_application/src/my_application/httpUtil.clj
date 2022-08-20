2f17910e362807ad571eb07a5e36255efec45164
ljldgup <1142509832@qq.com>
2022-08-21 01:42:59 +0800

    -

---
+++ clojure/my-application/my_application/src/my_application/httpUtil.clj
@@ -0,0 +1,69 @@
+(ns my-application.httpUtil
+  (:require [my-application.SysUtil :refer :all]))
+
+;使用apache httpclient :dependencies [[org.apache.httpcomponents/httpclient "4.5.3"]]
+(import
+  org.apache.http.impl.conn.PoolingHttpClientConnectionManager
+  org.apache.http.client.config.RequestConfig
+  ;引入同包下多个
+  (org.apache.http.impl.client CloseableHttpClient HttpClients)
+  java.util.concurrent.TimeUnit)
+
+(import org.apache.http.client.methods.HttpGet)
+(import org.apache.http.client.methods.HttpPost)
+(import org.apache.http.util.EntityUtils)
+
+(def connectionManager (PoolingHttpClientConnectionManager. 3 (TimeUnit/MINUTES)))
+(doto connectionManager
+  (.setMaxTotal 50)
+  (.setDefaultMaxPerRoute 5))
+
+(def requestConfig
+  (.build
+    (doto (RequestConfig/custom)
+      (.setConnectionRequestTimeout 5000)
+      (.setConnectTimeout 5000)
+      (.setSocketTimeout 12000))))
+
+(def pooledClient
+  (.build
+    (doto (HttpClients/custom)
+      (.setConnectionManager connectionManager)
+      (.setDefaultRequestConfig requestConfig))))
+
+
+(defn get_related_path_name [url]
+  (let
+    [relate_path (re-find #"(?<=//).+/(?=[^/]*$)", url)
+     file_name (re-find #"(?<=/)[^/]+$", url)]
+    (if (nil? file_name)
+      (list relate_path "index.html")
+      (list relate_path file_name))))
+(get_related_path_name "https://www.baidu.com/")
+
+(defn get_http [url]
+  (let [httpGet (HttpGet. url)]
+    (with-open [response (->> (.execute pooledClient httpGet) .getEntity .getContent)]
+      (EntityUtils/toString ((.getEntity response), "utf-8")))))
+
+
+(defn get_with_cache [url]
+  (println 'binary_save url)
+
+  (let [[relate_path file_name] (get_related_path_name url)
+        path_name (str relate_path file_name)]
+    (if (not (exists path_name))
+      (do
+        (mkdirs relate_path)
+        (let [httpGet (HttpGet. url)]
+          (println "write into " path_name)
+          (with-open [response (->> (.execute pooledClient httpGet) .getEntity .getContent)
+                      os (clojure.java.io/output-stream (str relate_path file_name))]
+            (clojure.java.io/copy response os))))
+      (println "exists" path_name))
+    path_name))
+
+(defn cache_return_string [url]
+  (slurp (get_with_cache url)))
+
+(cache_return_string "https://www.baidu.com/")
---
+++ clojure/my-application/my_application/src/my_application/m3u8_downloader.clj
@@ -0,0 +1,70 @@
+(ns my-application.m3u8-downloader
+  (:require [my-application.httpUtil :refer :all]))
+
+(def piece_count_limit 1000)
+(def out_put_file "cmd.txt")
+
+(def basic_url "https://3bmmikh.life/new/hls")
+
+
+(defn get_base_url [url]
+  (println 'get_base_url url)
+  (re-find #".+/(?=[^/]+$)", url))
+
+;(get_base_url "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
+;(get_base_url "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
+
+
+
+(defn get_url_name [url]
+  (let [respose (cache_return_string url)]
+    (list
+      (re-find #"[^\"]+index.m3u8" respose)
+      (re-find #"(?<=正在播放：)[^<]+", respose))))
+;(get_url_name "https://3bmmnr0e.life/suoyoushipin/guochan/29796.html")
+
+
+
+(defn get_vedio_path [origin_url]
+  (println 'get_vedio_path origin_url)
+  (->> (cache_return_string origin_url) (re-seq #".+index.m3u8") sort last))
+
+;(get_vedio_path "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
+
+(defn get_ts_paths [vedio_url]
+  (->> (cache_return_string vedio_url) (re-seq #".+\.ts")))
+;(get_ts_paths "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/700kb/hls/index.m3u8")
+
+(defn get_ffmpeg_cmd [base_path title]
+  (format "ffmpeg -i ./%sindex.m3u8 -c copy \"%s.mp4\"\r\n" base_path title))
+(get_ffmpeg_cmd "sdfsd" "山擦")
+
+
+
+(defn download_m3u8 [url title]
+  (let [vedio_base_url (get_base_url url)
+        [local_path _] (get_related_path_name url)
+        vedio_path (get_vedio_path url)
+        vedio_url (str vedio_base_url vedio_path)
+        ts_base_url (get_base_url vedio_url)
+        ts_paths (get_ts_paths vedio_url)]
+    (map println (list url vedio_base_url vedio_url ts_base_url))
+    (if (< (count ts_paths) piece_count_limit)
+      (do
+        (spit out_put_file (get_ffmpeg_cmd local_path title) :append true)
+        (doseq [ts_path ts_paths]
+          ;(Thread/sleep (rand-int rand_delay_limit))
+          (cache_return_string (str ts_base_url ts_path)))))))
+;(download_m3u8 "https://3bmmikh.life/new/hls/c6f169e6c2ff41109822d0187179eb69/index.m3u8")
+
+(defn download_mimei [url]
+  (let [[relate_index_url title] (get_url_name url)
+        index_url (str basic_url relate_index_url)]
+    (download_m3u8 index_url title)))
+
+;(download_mimei "https://3bmmnr0e.life/suoyoushipin/guochan/29796.html")
+
+
+(defn download_mimeis [url_list]
+  (spit out_put_file "")
+  (doseq [url url_list] (download_mimei url)))
