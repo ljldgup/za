10c6c1681fb4aeaf1f23a5a33549699673c4f403
ljldgup <1142509832@qq.com>
2022-11-28 23:47:05 +0800

    -

--- clojure/my_project/project.clj
+++ clojure/my_project/project.clj
@@ -1,6 +1,6 @@
-(defproject m3u8-downloader "0.1.0-SNAPSHOT"
+(defproject my-application "0.1.0-SNAPSHOT"
   :dependencies 
   [[org.clojure/clojure "1.10.3"] 
   [org.apache.httpcomponents/httpclient "4.5.3"] 
   [com.alibaba/fastjson "1.2.83"]]
-  :repl-options {:init-ns my-application.m3u8-downloader})
+  :repl-options {:init-ns web.m3u8-downloader})
